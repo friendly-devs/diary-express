4d7e7c0b1a75ab1c33f5443f3d3706f7bf27b733
tamvv <vovantam.dev@gmail.com>
2021-06-07 22:39:11 +0700

    Update sign up page

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const db = require('../models');
 const logger = require('../utils/logger');
 
@@ -9,25 +10,38 @@ router.get('/sign-in', (req, res) => {
   res.render('users/sign-in');
 });
 
+router.post('/sign-in', async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  const result = bcrypt.compareSync(password, user.password);
+
+  res.redirect('/');
+});
+
 router.get('/sign-up', (req, res) => {
   res.render('users/sign-up');
 });
 
-router.post('/sign-up', (req, res) => {
-  logger.info(req);
-
+router.post('/sign-up', (req, res, next) => {
   const data = req.body;
 
-  const user = new User({
-    username: data.username,
-    password: data.password,
-    firstName: data.firstName,
-    lastName: data.lastName,
-  });
+  try {
+    const saltRounds = 10;
+    const hash = bcrypt.hashSync(data.password, saltRounds);
 
-  user.save();
+    const user = new User({
+      username: data.username,
+      password: hash,
+    });
 
-  res.render('users/sign-up');
+    user.save();
+
+    res.redirect('/sign-in');
+  } catch (error) {
+    logger.error(error);
+    next();
+  }
 });
 
 module.exports = router;
